90a9e5f2ab7cabde210169f18559aba83b25695a
Richie Arnold <richard@ambercouch.co.uk>
2023-03-02 10:54:46 +0000

    WAC - updates to template

--- loop-template.php
+++ loop-template.php
@@ -11,7 +11,7 @@ $handle_js = 'ac_testimonial_script';
 $list = 'enqueued';
 
 $the_content = apply_filters('the_content', get_the_content());
-$testimonial_title = (get_field('testimonial_title') != '') ? get_field('testimonial_title') : false ;
+$testimonial_title = (get_field('testimonial_title') != '') ? get_field('testimonial_title') : get_the_title() ;
 $testimonial_intro = get_field('testimonial_intro');
 $testimonial_body = get_field('testimonial_body');
 $testimonial_cite = get_field('testimonial_citation');
@@ -32,24 +32,17 @@ if (! wp_script_is( $handle, $list )) {
 <article id="post-<?php the_ID(); ?>" <?php post_class('c-ac-testimonial'); ?>>
   <div class="c-ac-testimonial__thumb">
 
+<!--         <header  class="entry-header c-ac-testimonial__header">-->
+<!--           <h2 class="entry-title c-ac-testimonial__heading">-->
+<!--             <span class="c-ac-testimonial__title">--><?php //echo $testimonial_title ?><!--</span>-->
+<!--           </h2>-->
+<!--         </header>-->
 
-
-
+    <?php if ( !empty($testimonial_intro) || $testimonial_intro != '' ) : ?>
       <blockquote class="c-ac-testimonial__content">
-          <?php if ( $testimonial_intro != '' ) : ?>
         <div class="c-ac-testimonial__intro">
           <?php echo $testimonial_intro ?>
         </div>
-          <?php endif; ?>
-
-          <?php if ($testimonial_title) :?>
-            <header  class="entry-header c-ac-testimonial__header">
-              <h2 class="entry-title c-ac-testimonial__heading">
-                <span class="c-ac-testimonial__title"><?php echo $testimonial_title ?></span>
-              </h2>
-            </header>
-          <?php endif; ?>
-
           <?php if ( !empty($testimonial_body) ) : ?>
         <div class="c-ac-testimonial__body" data-state="off" data-container="testimonial<?php echo $tesimonial_id ?>" >
               <?php echo $testimonial_body ?>
@@ -64,7 +57,7 @@ if (! wp_script_is( $handle, $list )) {
         </div>
       </blockquote>
 
-
+    <?php endif; ?>
   </div>
 </article>
 </li>
